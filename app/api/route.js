1a910a23e07cf6b3609de231c0bf9ede484e6fe5
Richard Essuman <ressuman001@gmail.com>
2024-08-23 20:42:55 +0000

    refactor: Remove unused code and update API route and middleware

---
+++ app/api/route.js
@@ -0,0 +1,12 @@
+export function GET(request) {
+  console.log(request);
+
+  //return Response.json()
+  return new Response("Hello!");
+}
+
+// export function POST(request) {
+//   console.log(request);
+
+//    return new Response("Hi!");
+// }
